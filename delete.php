f03158e1c450abc3a52cfba0dcab59d5c6b22a8c
DharshanKumar <darshu260592@gmail.com>
2021-02-12 10:36:00 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -2,7 +2,11 @@
 $id = $_GET['id'];
 if( !empty( $id ) ){
 	require_once('dbconnection.php');
-	$sql = "DELETE FROM students WHERE id = $id";
+	$table = "students";
+	if( !empty( $_GET['table'] ) ){
+		$table = "course";
+	}
+	$sql = "DELETE FROM $table WHERE id = $id";
 	if ( $dbhandle->query( $sql ) ) {
 		header('Location: studentindex.php');
 	} else {
